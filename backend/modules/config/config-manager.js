f08cff609f775c1dfbe668faff9b32512c7684b9
Eugene Tree <me.eugenetree@gmail.com>
2025-01-25 18:16:13 +0200

    update config manager

--- backend/modules/config/config-manager.js
+++ backend/modules/config/config-manager.js
@@ -5,18 +5,18 @@ class ConfigManager {
   #config = null;
 
   async init() {
+    if (path.basename(process.cwd()) !== "backend") {
+      throw new Error(
+        "index.js file should be run from 'backend' folder via 'node index.js'",
+      );
+    }
+
     this.#config = await JSON.parse(
       await fs.readFile(path.resolve(process.cwd(), "./config.json")),
     );
 
     if (!this.#config?.lang) {
       throw new Error("'lang' property is not specified in 'config.js'");
-    }
-
-    if (path.basename(process.cwd()) !== "backend") {
-      throw new Error(
-        "index.js file should be run from 'backend' folder via 'node index.js'",
-      );
     }
 
     if (!path.resolve(process.cwd(), "../storage/cookies.json")) {
@@ -46,6 +46,27 @@ class ConfigManager {
         "videos language in 'videos.json' doesn't match with specified lang in config",
       );
     }
+
+    const remotionFiles = [
+      "check",
+      "level_a1",
+      "level_a2",
+      "level_b1",
+      "level_b2",
+      "level_c1",
+      "level_c2",
+      "outro.mp3",
+    ];
+
+    const remotionFilesCheck = remotionFiles.map((file) =>
+      fs.access(path.resolve(process.cwd(), `../public/${file}.mp3`)),
+    );
+
+    await Promise.all(remotionFilesCheck).catch(() => {
+      throw new Error(
+        `something wrong with one of these files: ${remotionFiles}`,
+      );
+    });
   }
 }
 
