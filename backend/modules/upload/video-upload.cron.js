36ea359ef8dab4b1f2a217410f9723c4dae64c3c
Eugene Tree <me.eugenetree@gmail.com>
2025-02-04 11:02:56 +0100

    gg

--- backend/modules/upload/video-upload.cron.js
+++ backend/modules/upload/video-upload.cron.js
@@ -12,35 +12,35 @@ const shouldRun = () => {
 
 const logger = new Logger("video-upload-cron");
 
-cron.schedule("* * * * *", async () => {
+cron.schedule("0 18,5 * * *", async () => {
   logger.info("cron is triggered");
 
-  if (!shouldRun()) {
-    logger.info("cron is skipped as it is not an 5/10/15/20... minute");
-    return;
-  }
+  // if (!shouldRun()) {
+  //   logger.info("cron is skipped as it is not an 5/10/15/20... minute");
+  //   return;
+  // }
 
-  if (!resourceOrchestrator.isAvailable()) {
-    logger.info(`orchestrator is busy.`);
-    return;
-  }
+  // if (!resourceOrchestrator.isAvailable()) {
+  //   logger.info(`orchestrator is busy.`);
+  //   return;
+  // }
 
   const latestUploadedVideo =
     await videoRepository.getLatestUploadedToTiktokVideo();
 
   logger.info(`latest uploaded video: ${latestUploadedVideo?.id}`);
 
-  if (
-    latestUploadedVideo &&
-    new Date() - new Date(latestUploadedVideo.tiktokUploadDate) <
-      1000 * 60 * 60 * 6
-  ) {
-    logger.info(
-      `latest video was uploaded less than 6 hours ago, no need to upload another one`,
-    );
+  // if (
+  //   latestUploadedVideo &&
+  //   new Date() - new Date(latestUploadedVideo.tiktokUploadDate) <
+  //     1000 * 60 * 60 * 6
+  // ) {
+  //   logger.info(
+  //     `latest video was uploaded less than 6 hours ago, no need to upload another one`,
+  //   );
 
-    return;
-  }
+  //   return;
+  // }
 
   const videoForUpload =
     await videoRepository.getOldestRenderedNotUploadedVideo();
@@ -50,7 +50,7 @@ cron.schedule("* * * * *", async () => {
     return;
   }
 
-  resourceOrchestrator.acquireLock();
+  // resourceOrchestrator.acquireLock();
   logger.info(`video #${videoForUpload.id} upload started`);
 
   const videoPath = path.resolve(
@@ -76,7 +76,7 @@ cron.schedule("* * * * *", async () => {
     logger.error(
       `upload of video ${videoForUpload.id} failed: ${error.message}`,
     );
-    await resourceOrchestrator.releaseLock();
+    // await resourceOrchestrator.releaseLock();
     return;
   }
 
@@ -85,7 +85,7 @@ cron.schedule("* * * * *", async () => {
   );
 
   await videoRepository.markAsUploadedToTiktok({ id: videoForUpload.id });
-  await resourceOrchestrator.releaseLock();
+  // await resourceOrchestrator.releaseLock();
 
   logger.info(`video marked as uploaded to tiktok: ${videoForUpload.id}`);
 });
